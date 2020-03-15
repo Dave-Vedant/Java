dae6e3b26cb7d32158b1ee231be46b78a0908d78
vedantdave77 <vedantdave77@gmail.com>
2020-03-14 23:42:02 -0400

    Song_List-modified version with interface
    src code of song list with interface fundametals, properly revise interface concept

---
+++ Interface/src/vedant/learningPhase/GearBox.java
@@ -0,0 +1,9 @@
+package vedant.learningPhase;
+
+public class GearBox {
+    private boolean clutchInOrOut;
+
+    public void operateClutch(boolean inOrOut){
+        this.clutchInOrOut = inOrOut;
+    }
+}
